52aad70ffbfc359e0ccdbd68f1a71e5be3810f7a
Matheus De O Zacanini <zacanini2@gmail.com>
2024-11-20 15:58:09 -0300

    :recycle: refactor : modularizando o roteamento das paginas , tirei a responsabilidade de roteamento das paginas do App.js , criando uma pasta Routers com um arquivo Router.js , que agora é o responsavel pelo roteamento

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Produto from './pages/Produto';
-import Categoria from './pages/Categoria';
-import Movimentacao from './pages/Movimentacao';
-import Usuario from './pages/Usuario';
 import './global.css';
-import Home from './pages/Home';
+import AppRouter from './routers/router';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/produtos" element={<Produto />} />
-                <Route path="/categorias" element={<Categoria />} />
-                <Route path="/movimentacoes" element={<Movimentacao />} />
-                <Route path="/usuarios" element={<Usuario />} />
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
-    );
+    return <AppRouter />;
 };
 
 export default App;
---
+++ frontend/src/routers/Router.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Produto from '../pages/Produto';
+import Categoria from '../pages/Categoria';
+import Movimentacao from '../pages/Movimentacao';
+import Usuario from '../pages/Usuario';
+import Home from '../pages/Home';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/produtos" element={<Produto />} />
+                <Route path="/categorias" element={<Categoria />} />
+                <Route path="/movimentacoes" element={<Movimentacao />} />
+                <Route path="/usuarios" element={<Usuario />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
